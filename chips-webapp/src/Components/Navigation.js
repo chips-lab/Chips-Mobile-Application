f2b4a19c6fd2aa587d93cadf58927677cce4c0f0
walka4d <brianwalker4d@gmail.com>
2020-07-30 08:28:44 -0500

    some pages initial setup

---
+++ chips-webapp/src/Components/Navigation.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Navigation extends Component {
+    render() {
+        return (
+            <div>
+                Nav
+            </div>
+        );
+    }
+}
+
+export default Navigation;
---
+++ chips-webapp/src/Config/Fire.js
@@ -0,0 +1,25 @@
+import * as firebase from 'firebase';
+import "firebase/auth";
+import "firebase/firestore";
+
+
+const firebaseConfig = firebase.initializeApp({
+    apiKey: "AIzaSyBxmpyzMiKMrx6hPRAu16DJSWXQyz9HmUY",
+    authDomain: "chips-5380b.firebaseapp.com",
+    databaseURL: "https://chips-5380b.firebaseio.com",
+    projectId: "chips-5380b",
+    storageBucket: "chips-5380b.appspot.com",
+    messagingSenderId: "869618511922",
+    appId: "1:869618511922:web:51917c43d75b74f0f6cc2b",
+    measurementId: "G-3ZRZFS165D"
+  });
+
+  class Firebase {
+    constructor() {
+    
+      this.auth = app.auth();
+      this.db = app.firestore();
+    }
+
+  }
+  export default new Firebase();
